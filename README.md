79556ad396f0843233560d84fbce0dd13377c843
Yeshey <41551785+Yeshey@users.noreply.github.com>
2025-06-24 18:31:13 +0100

    added nix stuff and virtual environment stuff

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # FMAP-Project
 "traduzir" para tensorflow/keras o model DQN que está no notebook 9
+
+# Make virtual environment python
+
+- If in NixOS, you should have flakes, activate the environment (`nix develop` or `direnv allow`) and run `sh venvnix.sh`
+
+1. python -m venv .venv --copies
+2. Source the environment:
+   1. **Linux:** source .venv/bin/activate
+   2. **Windows CMD** .venv\Scripts\activate
+   3. **Windows Powershell:** .venv\Scripts\Activate.ps1
+3. pip install --upgrade pip
+4. pip install -r requirements.txt
---
+++ flake.nix
@@ -0,0 +1,83 @@
+# https://github.com/nix-community/dream2nix/tree/main
+
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = { systems, nixpkgs, ... } @ inputs:
+  let
+    eachSystem = f:
+      nixpkgs.lib.genAttrs (import systems) (
+        system:
+          f nixpkgs.legacyPackages.${system}
+      );
+  in {
+    devShells = eachSystem (pkgs: 
+    let
+      #lt = pkgs.python311Packages.buildPythonPackage {
+      #  pname = "lt_core_news_sm";
+      #  version = "3.7.0";
+      #  src = pkgs.fetchurl {
+      #    url = "https://github.com/explosion/spacy-models/releases/download/pt_core_news_sm-3.7.0/pt_core_news_sm-3.7.0.tar.gz";
+      #    hash = "sha256-uG0vywIrvh3/qGkoUskJRZHpPQKZtlxb35L3lArixHw=";
+      #  };
+      #  buildInputs = with pkgs.python311Packages; [ pipBuildHook ];
+      #  dependencies = with pkgs.python311Packages; [ spacy ];
+      #};
+      #dontCheckPython = drv: drv.overridePythonAttrs (old: { doCheck = false; });
+    in {
+      default = pkgs.mkShell {
+        packages = [
+          #pkgs.jupyter-all
+          (pkgs.python312.withPackages (python-pkgs: with python-pkgs; [
+            tensorflow
+            pytorch
+            keras
+            pandas
+            numpy
+            jupyter
+            matplotlib
+            seaborn
+            nltk # (NLP processing)
+            spacy # (NLP processing)
+            # lt
+            #progressbar
+            #transformers
+            #datasets
+
+            # script andre
+            #selenium
+            #webdriver-manager
+            #beautifulsoup4
+            #tqdm
+
+            jupyter
+            jupyter-collaboration
+            jupyterlab-git
+
+            #torch
+
+            # Add version overrides for conflicting dependencies
+            #(protobuf.overridePythonAttrs (old: { version = "4.25.3"; }))
+            #(typing-extensions.overridePythonAttrs (old: { version = "4.9.0"; }))
+            #(numpy.override { blas = pkgs.openblasCompat; })
+          ]))
+          pkgs.virtualenv
+          #pkgs.chromedriver
+        ];
+
+        shellHook = ''
+          echo "Development shell ready!"
+          echo "Run with something like:"
+          echo "jupyter lab --ip=0.0.0.0 --port=8891 --no-browser --ContentsManager.allow_hidden=True --collaborative"
+
+          echo "when running if you see this on the browser: http://127.0.0.1:8891/tree?token=6c86ed65bba3efd8ebd779bb4263094bbaaa4d9c28a648f4, that means that if you want to use the jupyternotebook server as a kernel for VSC, you need to use 6c86ed65bba3efd8ebd779bb4263094bbaaa4d9c28a648f4 as the password"
+
+          # jupyter notebook
+        '';
+      };
+    });
+  };
+}
